5c5ab2634ed04f901bc1ed070871a48140829e3b
Jared Fraser <j.fraser@modsognir.com>
2012-02-15 10:34:24 +1100

    Fixed current_first ordering

--- lib/ranked-model/ranker.rb
+++ lib/ranked-model/ranker.rb
@@ -209,7 +209,7 @@ module RankedModel
             _finder = _finder.where \
               instance.class.arel_table[:id].not_eq(instance.id)
           end
-          _finder.order(instance.class.arel_table[ranker.column].asc).select([:id, ranker.column])
+          _finder.order(instance.class.arel_table[ranker.column].asc.to_sql).select([:id, ranker.column])
         end
       end
 
